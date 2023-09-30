9523399ba2e0d3a4ff450bd6f142ca329fa1eb72
Abdul Rasheed <ar3472560@gmail.com>
2023-09-30 19:29:29 +0500

    second code for age changing

--- gitsecond.py
+++ gitsecond.py
@@ -7,9 +7,9 @@ while True:
         print("Thanks You.")
         break
     age = int(age)
-    if age <= 5:
+    if age <= 3:
         print("The Ticket cost 10$.")
-    elif age <= 16 and age > 5:
+    elif age <= 13 and age > 3:
         print("The Ticket cost is 20$.")
     else:
         print("The Ticket cost is 30$.")
